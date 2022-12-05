dd18f912da7a8676da5f77d151e8bbe86d69ba69
Vincent Casal <DS7K@eurofins.com>
2022-12-05 14:50:25 +0100

    day05-part1

---
+++ day-05/script.py
@@ -0,0 +1,61 @@
+"Advent of code day 5."
+import re;
+
+def main():
+    handle = open(r'./day-05/input', 'r', encoding='UTF8')
+    _lines = handle.readlines()
+    _cleanup = SupplyStacks(_lines)
+    print(_cleanup.answer())
+
+
+class SupplyStacks:
+    "Camp cleanup class."
+
+    def __init__(self, lines) -> None:
+        index = 0
+        stackPattern = r"^\s*(\[\w\]\s*)+$"
+        stacksLines = []
+        while index < len(lines) and re.match(stackPattern, lines[index]):
+            stacksLines.append(lines[index])
+            index += 1 
+        stacksNumberLine = lines[index]
+        index+=1
+
+        numberOfStacks = len(stacksNumberLine.split())
+        self.stacks = []
+        for _ in range(0, numberOfStacks):
+            self.stacks.append([])
+        
+        for line in stacksLines:
+            for stack in range(0, numberOfStacks):
+                element = line[stack*4 : stack*4 + 4].strip()
+                if element:
+                    self.stacks[stack].insert(0, element[1])
+
+        movePattern = r"^move (\d+) from (\d+) to (\d+)$"
+        while index < len(lines) and not re.match(movePattern, lines[index]):
+            index += 1
+        self.moveLines = []
+        while index < len(lines) and re.match(movePattern, lines[index]):
+            self.moveLines.append(lines[index])
+            index+=1        
+
+    def answer(self):
+        movePattern = r"^move (\d+) from (\d+) to (\d+)$"
+        for move in self.moveLines:
+            m = re.match(movePattern, move)
+            nbofmove = int(m.group(1))
+            fromstack = int(m.group(2))
+            tostack = int(m.group(3))
+            for _ in range(0, nbofmove):
+                self.stacks[tostack - 1].append(self.stacks[fromstack-1].pop())
+        result = ''
+        for stack in self.stacks:
+            result += stack[-1]
+        return result
+
+
+if __name__ == '__main__':
+    main()
+pass
+
---
+++ day-05/script.tests.py
@@ -0,0 +1,25 @@
+"Test Round class from scrip."
+import unittest
+
+from script import SupplyStacks
+
+
+class TestSupplyStacks(unittest.TestCase):
+    "Tests for RucksackReorganization"
+
+    def test_example(self):
+        _example = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+        _impl = SupplyStacks(_example.splitlines())
+        self.assertEqual(_impl.answer(), 'CMZ')
+
+if __name__ == '__main__':
+    unittest.main()
+pass

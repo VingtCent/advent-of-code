ec0daaf5c55a0d41a6bcc8920c04b12d29b652b5
Vincent Casal <DS7K@eurofins.com>
2022-12-09 16:45:53 +0100

    day09-part2

--- day-09/script.py
+++ day-09/script.py
@@ -18,7 +18,6 @@ class RopeBridge:
 
     def answer1(self):
         "Returns Answer to part1 problem"
-
         positions = set()
         rope = Rope()
 
@@ -30,18 +29,24 @@ class RopeBridge:
 
     def answer2(self):
         "Answer to part 2."
-        return None
+        positions = set()
+        rope = Rope(9)
+
+        for move in self.moves:
+            for _ in range(0, move[1]):
+                positions.add(rope.move(move[0]))
+
+        return len(positions)
 
 
 class Rope:
     "Define the behavior of a rope."
 
-    def __init__(self) -> None:
-        self._head = [0, 0]
-        self._tail = [0, 0]
+    def __init__(self, tails=1) -> None:
+        self._elements = [[0, 0]] + [[0, 0] for _ in range(0, tails)]
 
     @staticmethod
-    def _move(direction, position):
+    def _dostep(direction, position):
         match direction:
             case "U":
                 position[1] += 1
@@ -54,18 +59,22 @@ class Rope:
             case _:
                 raise ValueError(direction)
 
+    @staticmethod
+    def _follow(first, second):
+        xdiff = first[0] - second[0]
+        ydiff = first[1] - second[1]
+        if abs(xdiff) > 1 or abs(ydiff) > 1:
+            if xdiff != 0:
+                second[0] += xdiff // abs(xdiff)
+            if ydiff != 0:
+                second[1] += ydiff // abs(ydiff)
+
     def move(self, direction):
         "Move the rope to one direction."
-        Rope._move(direction, self._head)
-        xdiff = self._head[0] - self._tail[0]
-        ydiff = self._head[1] - self._tail[1]
-        if abs(xdiff) > 1 or abs(ydiff) > 1:
-            if xdiff == 0 or ydiff == 0:
-                Rope._move(direction, self._tail)
-            else:
-                self._tail[0] += xdiff // abs(xdiff)
-                self._tail[1] += ydiff // abs(ydiff)
-        return (self._tail[0], self._tail[1])
+        Rope._dostep(direction, self._elements[0])
+        for i in range(1, len(self._elements)):
+            Rope._follow(self._elements[i - 1], self._elements[i])
+        return (self._elements[-1][0], self._elements[-1][1])
 
 
 if __name__ == "__main__":
--- day-09/script.test.py
+++ day-09/script.test.py
@@ -24,8 +24,16 @@ R 2"""
 
     def test_answer2(self):
         "Test answer for part 2."
-        _impl = RopeBridge(self.example.splitlines())
-        self.assertEqual(_impl.answer2(), None)
+        example2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+        _impl = RopeBridge(example2.splitlines())
+        self.assertEqual(_impl.answer2(), 36)
 
 
 if __name__ == "__main__":

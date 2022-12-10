99347ac7cbc6376511a4408d02ad59454f9a844c
VingtCent <vincent.casal@gmail.com>
2022-12-10 12:57:14 +0100

    day10-part2

--- day-10/script.py
+++ day-10/script.py
@@ -14,32 +14,57 @@ class CathodeRayTube:
     "Impl class."
 
     def __init__(self, lines) -> None:
-        self.commands = lines
+        self._commands = lines
+        self._xvalue = 1
+        self._cycle = 0
 
     def answer1(self):
         "Returns Answer to part1 problem"
-        xvalue = 1
-        cycle = 0
+        self._xvalue = 1
+        self._cycle = 0
         nextmeasurment = 20
         signalstrength = 0
-        for command in [c.split() for c in self.commands]:
+        for command in [c.split() for c in self._commands]:
             match command[0]:
                 case "noop":
-                    cycle += 1
+                    self._cycle += 1
                 case "addx":
-                    cycle += 2
-                    nextvalue = xvalue + int(command[1])
+                    self._cycle += 2
+                    nextvalue = self._xvalue + int(command[1])
                 case _:
                     raise ValueError(f"Command {command[0]} not handle")
-            if cycle >= nextmeasurment:
-                signalstrength += nextmeasurment * xvalue
+            if self._cycle >= nextmeasurment:
+                signalstrength += nextmeasurment * self._xvalue
                 nextmeasurment += 40
-            xvalue = nextvalue
+            self._xvalue = nextvalue
 
         return signalstrength
 
+    def _print(self):
+        self._cycle += 1
+        if (self._xvalue) <= self._cycle % 40 <= (self._xvalue + 2):
+            print("#", sep='', end='')
+        else:
+            print(".", sep='', end='')
+        if self._cycle % 40 == 0:
+            print("\n", sep='', end='')
+
     def answer2(self):
         "Answer to part 2."
+        self._xvalue = 1
+        self._cycle = 0
+        print("Display:")
+        for command in [c.split() for c in self._commands]:
+            match command[0]:
+                case "noop":
+                    self._print()
+                case "addx":
+                    for _ in range(0, 2):
+                        self._print()
+                    self._xvalue = self._xvalue + int(command[1])
+                case _:
+                    raise ValueError(f"Command {command[0]} not handle")
+
         return None
 
 
--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ https://adventofcode.com/2022
 
 ## day 07
 Use a solution to see it was the size of the folder and not it's name that is expected...
+
+## day 10
+Remember to re-initialize object state between steps.
+Try to debu before thinking there is a bug in the problem!!

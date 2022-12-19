133ee8ffbedc7b72ea6e817d74821444762e809c
Vincent Casal <DS7K@eurofins.com>
2022-12-19 14:34:46 +0100

    day16-solution

--- day-18/script.py
+++ day-18/script.py
@@ -1,4 +1,4 @@
-"Advent of code day 17."
+"Advent of code day 16."
 
 import re
 from typing import Tuple
@@ -8,13 +8,13 @@ def main():
     "Main."
     handle = open(r"./day-16/input", "r", encoding="UTF8")
     _lines = handle.readlines()
-    _impl = PyroclasticFlow(_lines)
+    _impl = ProboscideaVolcanium(_lines)
     print("Answer 1:", _impl.answer1())
-    _impl = PyroclasticFlow(_lines)
+    _impl = ProboscideaVolcanium(_lines)
     print("Answer 2:", _impl.answer2())
 
 
-class PyroclasticFlow:
+class ProboscideaVolcanium:
     "Impl class."
 
     def __init__(self, lines: list[str]) -> None:
--- day-18/script.test.py
+++ day-18/script.test.py
@@ -1,23 +1,23 @@
 "Test scrip file."
 import unittest
-from script import PyroclasticFlow
+from script import ProboscideaVolcanium
 
 
-class TestPyroclasticFlow(unittest.TestCase):
-    "Tests for PyroclasticFlow"
+class TestBeaconExclusionZone(unittest.TestCase):
+    "Tests for ProboscideaVolcanium"
 
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
-        self.example = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
+        self.example = """"""
 
     def test_answer1(self):
         "Test answer for part 1."
-        _impl = PyroclasticFlow(self.example.splitlines())
-        self.assertEqual(_impl.answer1(), 3068)
+        _impl = ProboscideaVolcanium(self.example.splitlines())
+        self.assertEqual(_impl.answer1(), None)
 
     def test_answer2(self):
         "Test answer for part 2."
-        _impl = PyroclasticFlow(self.example.splitlines())
+        _impl = ProboscideaVolcanium(self.example.splitlines())
         self.assertEqual(_impl.answer2(), None)
 
 
